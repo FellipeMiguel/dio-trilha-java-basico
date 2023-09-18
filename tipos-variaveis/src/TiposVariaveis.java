100786dfbada877b88c2c771d7c7465fe3081a15
FellipeMiguel <fellipemiguel1@icloud.com>
2023-09-18 13:20:14 -0300

    Tipos e Variáveis finalizada

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,20 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+
+        //string
+        String meuNome = "Fellipe Miguel";
+
+        double salarioMinimo = 2500.33;
+
+        // variáveis podem ser reatribuídas
+        int numero = 1;
+        numero = 2;
+
+        System.out.println(numero);
+
+        // variáveis não podem ser reatribuídas quando usamos final
+        final double VALOR_DE_PI = 3.14;
+
+
+    }
+}
