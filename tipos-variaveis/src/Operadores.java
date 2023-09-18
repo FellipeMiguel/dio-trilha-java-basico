7126303fe8d06add0ad206a5b264f22e6285a757
FellipeMiguel <fellipemiguel1@icloud.com>
2023-09-18 16:28:48 -0300

    Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+  public static void main(String[] args) {
+    boolean condition1 = true;
+    boolean condition2 = false;
+
+    if (condition1 && condition2) System.out.println("Both conditions are true");
+    if (condition1 || condition2) System.out.println("One condition is true");
+
+    System.out.println("The end");
+  }
+}
