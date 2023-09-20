ecc2cd6eb30659a7d2554a17d180951ceabeacb5
FellipeMiguel <fellipemiguel1@icloud.com>
2023-09-20 13:44:52 -0300

    Estrutura de repetição For

---
+++ estrutura-repeticao/src/BrakeContinueExample.java
@@ -0,0 +1,8 @@
+public class BrakeContinueExample {
+    public static void main(String[] args) {
+        for (int number = 1; number <= 5; number++){
+            if(number == 3) continue;
+            System.out.println(number);
+        }
+    }
+}
---
+++ estrutura-repeticao/src/ForArrayExample.java
@@ -0,0 +1,9 @@
+public class ForArrayExample {
+    public static void main(String[] args) {
+        String[] students = {"Fellipe", "Ester", "Fernando", "Fernanda"};
+
+        for(String student : students) {
+            System.out.println("Student name is: " + student);
+        }
+    }
+}
